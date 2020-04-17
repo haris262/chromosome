7f2b990393f9d53775523e8de1da32e8b9924dee
haris262 <hariscengic262@outlook.com>
2020-04-17 13:27:07 +0200

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Header.css";
+
+
+
+
+const Header = () => {
+
+    return(
+      <div className="container">
+          <div className="item">
+              <a className="item-anchor">
+                  Menu
+              </a>
+          </div>
+          <div className="item">
+              <a className="item-anchor">
+                  Menu
+              </a>
+          </div>
+          <div className="item">
+              <a className="item-anchor">
+                  Menu
+              </a>
+          </div>
+      </div>
+    );
+}
+
+
+export default Header;
