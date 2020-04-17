d88936220c790176f796a74207f0de53d67c2400
haris262 <hariscengic262@outlook.com>
2020-04-17 14:54:02 +0200

    got ideogram working(had to downgrade)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
+
+import Ideogram from 'ideogram';
 import Header from "./components/Header/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      Hello World
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <AppIdeogram/>
+      </div>
+    );
+  }
+}
+
+class AppIdeogram extends Component {
+
+  componentDidMount() {
+    return new Ideogram({
+      organism: 'human',
+      annotations: [{
+        name: 'BRCA1',
+        chr: '17',
+        start: 43044294,
+        stop: 43125482
+      }],
+      container: '#ideo-container'
+    });
+  }
+
+  render() {
+    return (
+      <div id="ideo-container"></div>
+    );
+  }
 }
 
 export default App;
