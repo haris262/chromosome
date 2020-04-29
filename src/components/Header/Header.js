2d6d4fc368a4d3bc040514bf10fb3e713f266c3b
haris262 <hariscengic262@outlook.com>
2020-04-29 14:27:13 +0200

    modal partial

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Header.css";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
 
 
 
 
 const Header = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
 
     return(
+        <>
       <div className="container">
-          <div className="item">
+          <div className="item" onClick={handleShow}>
               <a className="item-anchor">
-                  Menu
+                  Add
               </a>
           </div>
           <div className="item">
@@ -23,7 +32,23 @@ const Header = () => {
                   Menu
               </a>
           </div>
+          <Modal show={show} onHide={handleClose} animation={false}>
+              <Modal.Header closeButton>
+                  <Modal.Title>Modal heading</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+              <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                      Close
+                  </Button>
+                  <Button variant="primary" onClick={handleClose}>
+                      Save Changes
+                  </Button>
+              </Modal.Footer>
+          </Modal>
+
       </div>
+            </>
     );
 }
 
